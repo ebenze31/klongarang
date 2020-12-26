74ed0018d73fc5bc0e3f68e2f553589a668d61d2
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2020-12-26 10:49:04 +0700

    update

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -3,67 +3,73 @@
    
   <header>
       <!-- Header Start -->
-    <div class="header-area" >
-          <div class="main-header ">
-              <!-- <div style="background-color:#CCFFFF " class="header-mid d-none d-md-block">
-                <div class="container">
-                      <div class="row d-flex align-items-center"> -->
-                          <!-- Logo -->
-                          <!-- <div class="col-xl-3 col-lg-3 col-md-3">
-                              <div class="logo">
-                                  <a href="{{ url('/home') }}"><img src="{{ asset('/img/klongarang/logologo.png') }}" alt=""></a>
-                              </div>
-                          </div>
-                          <div class="col-xl-9 col-lg-9 col-md-9">
-                              <div class="header-banner f-right ">
-                                  <img src="{{ asset('/img/klongarang/vru_logo_โครงการ.png') }}" alt="">
-                              </div>
-                          </div>
-                      </div>
+    <div class="header-area">
+            <div class="main-header ">
+                <div style="background-color:#a0ccee " class="header-mid d-none d-md-block">
+                   <div class="container">
+                        <div class="row d-flex align-items-center">
+                            <!-- Logo -->
+                            <div class="col-xl-3 col-lg-3 col-md-3">
+                                <div class="logo">
+                                    <a href="{{ url('/home') }}"><img src="{{ asset('/img/klongarang/logologo.png') }}" alt=""></a>
+                                </div>
+                            </div>
+                            <div class="col-xl-9 col-lg-9 col-md-9">
+                                <div class="header-banner f-right ">
+                                    <img src="{{ asset('/img/klongarang/vru_logo_โครงการ.png') }}" alt="">
+                                </div>
+                            </div>
+                        </div>
+                   </div>
                 </div>
-              </div> -->
-            <div style="background-color:#a0ccee " class="header-bottom header-sticky">
-                  <div class="container">
-                      <div class="row align-items-center">
-                          <div class="col-xl-10 col-lg-10 col-md-12 ">
-                              <!-- sticky -->
-                                  <div class="sticky-logo f-right">
-                                      <a href="{{ url('/home') }}"><img src="{{ asset('/img/klongarang/logologo.png') }}" alt=""></a>
-                                  </div>
-                              <!-- Main-menu -->
-                              <div class="main-menu d-md-block">
-                                  <nav>                  
-                                      <ul id="navigation">    
-                                          <li><a href="{{ url('/home') }}"><i style="color: #ed5565;" class="fas fa-home"></i> หน้าแรก</a></li>
-                                          <li><a href="{{ url('/about') }}"><i style="color: #197d4c;" class="fas fa-address-card"></i> เกี่ยวกับโครงการ</a></li>
-                                          <li><a href="{{ url('/aboutcommu') }}"><i style="color: #ad6234;" class="far fa-address-card"></i> เกี่ยวกับหมู่บ้าน</a></li>
-                                          <li><a href="{{ url('/products') }}"><i style="color: #922692;" class="fas fa-shopping-basket"></i> สินค้าและของที่ระลึก</a></li>
-                                          <li><a href="{{ url('/review') }}"><i style="color: #fdf744;" class="fas fa-star"></i></i> ดูรีวิว</a></li>
-                                          <li><a >ติดต่อ <i style="color: #FF4500;" class="fas fa-caret-down"></i> </a>
-                                              <ul class="submenu">
-                                                  <li><a href="https://www.facebook.com/%E0%B8%9A%E0%B9%89%E0%B8%B2%E0%B8%99%E0%B8%84%E0%B8%A5%E0%B8%AD%E0%B8%87%E0%B8%AD%E0%B8%B2%E0%B8%A3%E0%B8%B2%E0%B8%87-470519779751019" target="_blank"><i style="color: #1E90FF;" class="fab fa-facebook"></i> : บ้านคลองอาราง</a></li>
-                                                  <li><a href="mailto:bb@gmail.com" target="_blank"><i style="color: #FF0000;" class="fas fa-paper-plane"></i> : E-mail</a></li>
-                                                  <li><a href="tel:0623894316" target="_blank"><i style="color: #0099FF;" class="fas fa-phone-volume"></i> : 062-3894316</a></li>
-                                                  <li><a href="http://line.me/ti/p/~nongbenze012" target="_blank"><i style="color: #00CC33;" class="fab fa-line"></i> : ID:</a></li>
-                                              </ul>
-                                          </li>
-                                      </ul>
-                                  </nav>
-                              </div>
-                          </div>             
-                          <div class="col-xl-2 col-lg-2 col-md-4">
-                              <div class="header-right-btn f-right d-none d-lg-block">
-                              <img src="{{ asset('/img/klongarang/vru_logo_โครงการ.png') }}" alt="">
-                              </div>
-                          </div>
-                          <!-- Mobile Menu -->
-                          <div class="d-none col-12">
-                              <div class="mobile_menu "></div>
-                          </div>
-                      </div>
-                  </div>
+               <div style="background-color:#a0ccee " class="header-bottom header-sticky">
+                    <div class="container">
+                        <div class="row align-items-center">
+                            <div class="col-xl-10 col-lg-10 col-md-12 header-flex">
+                                <!-- sticky -->
+                                    <div class="sticky-logo">
+                                        <a href="{{ url('/home') }}"><img src="{{ asset('/img/klongarang/logologo.png') }}" alt=""></a>
+                                    </div>
+                                <!-- Main-menu -->
+                                <div class="main-menu d-none d-md-block">
+                                    <nav>                  
+                                        <ul id="navigation">    
+                                            <li><a href="{{ url('/home') }}"><i style="color: #ed5565;" class="fas fa-home"></i> หน้าแรก</a></li>
+                                            <li><a href="{{ url('/about') }}"><i style="color: #197d4c;" class="fas fa-address-card"></i> เกี่ยวกับโครงการ</a></li>
+                                            <li><a href="{{ url('/aboutcommu') }}"><i style="color: #ad6234;" class="far fa-address-card"></i> เกี่ยวกับหมู่บ้าน</a></li>
+                                            <li><a href="{{ url('/products') }}"><i style="color: #922692;" class="fas fa-shopping-basket"></i> สินค้าและของที่ระลึก</a></li>
+                                            <li><a href="{{ url('/review') }}"><i style="color: #fdf744;" class="fas fa-star"></i></i> ดูรีวิว</a></li>
+                                            <li><a >ติดต่อ <i style="color: #FF4500;" class="fas fa-caret-down"></i> </a>
+                                                <ul class="submenu">
+                                                    <li><a href="https://www.facebook.com/%E0%B8%9A%E0%B9%89%E0%B8%B2%E0%B8%99%E0%B8%84%E0%B8%A5%E0%B8%AD%E0%B8%87%E0%B8%AD%E0%B8%B2%E0%B8%A3%E0%B8%B2%E0%B8%87-470519779751019" target="_blank"><i style="color: #1E90FF;" class="fab fa-facebook"></i> : บ้านคลองอาราง</a></li>
+                                                    <li><a href="mailto:bb@gmail.com" target="_blank"><i style="color: #FF0000;" class="fas fa-paper-plane"></i> : E-mail</a></li>
+                                                    <li><a href="tel:0623894316" target="_blank"><i style="color: #0099FF;" class="fas fa-phone-volume"></i> : 062-3894316</a></li>
+                                                    <li><a href="http://line.me/ti/p/~nongbenze012" target="_blank"><i style="color: #00CC33;" class="fab fa-line"></i> : ID:</a></li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>             
+                            <div class="col-xl-2 col-lg-2 col-md-4">
+                                <div class="header-right-btn f-right d-none d-lg-block">
+                                    <i class="fas fa-search special-tag"></i>
+                                    <div class="search-box">
+                                        <form action="#">
+                                            <input type="text" placeholder="Search">
+                                            
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Mobile Menu -->
+                            <div class="col-12">
+                                <div class="mobile_menu d-block d-md-none"></div>
+                            </div>
+                        </div>
+                    </div>
+               </div>
             </div>
-          </div>
-    </div>
+       </div>
       <!-- Header End -->
   </header>
