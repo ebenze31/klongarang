ee8a24f3e6cb89bc1c72ddf0cc90b7bdc25a8f1f
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2020-12-28 14:19:31 +0700

    Update contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,6 +1,6 @@
 <div class="social-share pt-30">
     <div class="section-tittle">
-        <h4 class="mr-20">ติดต่อ :</h4>
+        <h3 class="mr-20">ติดต่อ:</h3>
         <ul>
             <li><a href="https://www.facebook.com/%E0%B8%9A%E0%B9%89%E0%B8%B2%E0%B8%99%E0%B8%84%E0%B8%A5%E0%B8%AD%E0%B8%87%E0%B8%AD%E0%B8%B2%E0%B8%A3%E0%B8%B2%E0%B8%87-470519779751019" target="_blank"><img src="{{ asset('/img/icon/icon-fb.png') }}" alt=""></a></li>
             <li><a href="http://line.me/ti/p/~0623894316" target="_blank"><img src="{{ asset('/img/icon/icon-line.png') }}" alt=""></a></li>
