0760f6d7ddc59eabfebd0af82dfb74672b7ee7c7
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2020-12-28 15:29:53 +0700

    Update show.blade.php

--- resources/views/travel/show.blade.php
+++ resources/views/travel/show.blade.php
@@ -8,7 +8,7 @@
             <div class="container">
                <div class="weekly-wrapper">
                     <div class="row">
-                        <div class="col-lg-12">
+                        <div class="col-12 news-poster d-none d-lg-block">
                             <div class="weekly-news-active dot-style d-flex dot-style">
                                 @php
                                     $photo_5 = $travel->photo_5;
@@ -41,6 +41,34 @@
                                 </div>
                                 @endif
                             </div><br>
+                        </div>
+
+                        <div id="carouselExampleIndicators" class="carousel slide d-block d-md-none" data-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img height="100%" width="100%" src="{{ url('storage')}}/{{ $travel->photo_1 }}" alt="">
+                                </div>
+                                <div class="carousel-item">
+                                    <img height="100%" width="100%" src="{{ url('storage')}}/{{ $travel->photo_2 }}" alt="">
+                                </div>
+                                <div class="carousel-item">
+                                    <img height="100%" width="100%" src="{{ url('storage')}}/{{ $travel->photo_3 }}" alt="">
+                                </div>
+                                <div class="carousel-item">
+                                    <img height="100%" width="100%" src="{{ url('storage')}}/{{ $travel->photo_4 }}" alt="">
+                                </div>
+                                <div class="carousel-item">
+                                    <img height="100%" width="100%" src="{{ url('storage')}}/{{ $travel->photo_5 }}" alt="">
+                                </div>
+                            </div>
+                            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                            </a>
                         </div>
                     </div>
                </div>
