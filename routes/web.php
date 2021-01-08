ff4c24d43f0b40cdf50d66f8d35117ff378c9f96
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2021-01-09 00:01:51 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home')->name('home');
 });
 Route::get('home', function () {
     return view('home');
